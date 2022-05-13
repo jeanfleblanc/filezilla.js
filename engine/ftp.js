b8b3f853151420b2ac812bd62d6c2fc5fad70b61
Jean-François Leblanc <74912921+Jean-Francois-Leblanc@users.noreply.github.com>
2022-05-13 10:46:48 -0400

    Add engine and some tests

--- engine/ftp.js
+++ engine/ftp.js
@@ -1,7 +1,48 @@
+const basicftp = require("basic-ftp")
+
+let host = () => {
+    return ""
+}
+
+let port = () => {
+    return ""
+}
+
 let username = () => {
     return ""
 }
 
+let password = () => {
+    return ""
+}
+
+let directoryContents = () => {
+    return "Files"
+}
+
+let connect = async () => {
+    const client = new basicftp.Client()
+    client.ftp.verbose = true
+    try {
+        await client.access({
+            host: host,
+            user: username,
+            password: password,
+            secure: true
+        })
+        console.log(await client.list())
+    }
+    catch(err) {
+        console.log(err)
+    }
+    client.close()
+}
+
 module.exports = {
-    username
+    host,
+    port,
+    username,
+    password,
+    connect,
+    directoryContents
 }
