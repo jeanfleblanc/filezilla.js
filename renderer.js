e5be0cd39a514ff01e39638a8e82bd38a2613d5a
Jean-François Leblanc <74912921+Jean-Francois-Leblanc@users.noreply.github.com>
2022-06-08 09:28:30 -0400

    Connect to engine

--- renderer.js
+++ renderer.js
@@ -4,3 +4,14 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+//const ftp = require("./engine/ftp.js")
+
+document.getElementById('button-connect').addEventListener('click', (event) => {
+
+    const host = document.getElementById('text-host').value
+    const port = document.getElementById('text-port').value
+
+    document.getElementById('remote-host').innerText = 'Connected to ' + host + ":" + port
+})
+
+document.getElementById('text-host').focus()
