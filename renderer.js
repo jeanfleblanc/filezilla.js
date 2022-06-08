be3d7e06363b5718751b9356d49c3662e5eced8e
Jean-François Leblanc <74912921+Jean-Francois-Leblanc@users.noreply.github.com>
2022-06-08 10:54:53 -0400

    Expose ftp to main world

--- renderer.js
+++ renderer.js
@@ -4,12 +4,13 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
-//const ftp = require("./engine/ftp.js")
-
 document.getElementById('button-connect').addEventListener('click', (event) => {
 
     const host = document.getElementById('text-host').value
     const port = document.getElementById('text-port').value
+
+    window.ftp.host = host
+    window.ftp.port = port
 
     document.getElementById('remote-host').innerText = 'Connected to ' + host + ":" + port
 })
